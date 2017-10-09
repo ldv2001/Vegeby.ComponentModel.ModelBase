8f571aaf6ea34270e01fb44ce024fa9e836a176d
Jens Vegeby <jens@vegeby.nu>
2017-10-09 23:45:48 +0200

    My initial code commit

---
+++ Vegeby.ComponentModel.ModelBase/ModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Vegeby.ComponentModel
+{
+    public abstract class ModelBase : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        protected virtual bool AdvertiseChanged => true;
+        protected virtual bool AdvertiseChanging => true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public void OnPropertyChanging([CallerMemberName] string propertyName = null) => PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+
+        public void SetProperty<T>(ref T destination, T value)
+        {
+            if (AdvertiseChanging) OnPropertyChanging();
+            destination = value;
+            if (AdvertiseChanged) OnPropertyChanged();
+        }
+    }
+}
